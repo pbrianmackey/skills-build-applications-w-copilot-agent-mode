909ae6b9b3b9a502b3e6e6c055bf40469afcbc13
Brian Mackey <mackey.brian@gmail.com>
2026-02-26 14:04:40 +0000

    Add initial Django project structure with essential files and configurations

---
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -0,0 +1,43 @@
+"""octofit_tracker URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import os
+
+# API Root View
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        'message': 'OctoFit Tracker API',
+        'version': '1.0.0'
+    })
+
+router = DefaultRouter()
+
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = "http://localhost:8000"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', api_root, name='api-root'),
+    path('api/', include(router.urls)),
+]
